b400a9def49b75c9cc8c1a4f22487802caf314b0
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-10-15 13:29:26 +0200

    update fetch-data.js

--- fetch-data.js
+++ fetch-data.js
@@ -1,32 +1,20 @@
-document.addEventListener("DOMContentLoaded",() => {
-    async function fetchUserData() {
-        const apiUrl = 'https://jsonplaceholdertypicode.com/user';
-        const dataContainer = document.getElementById('api-data');
-    try {
-        // fetch user data fron API
-        const response = await fetch(apiUrl);
-        const user = await response.json();
-        // Clear the "Loading user data..." message
-        dataContainer.innerHTML = '';
-        // Create a <ul> element
-        const userList = document.createElement('ul');
-        // Loop through the user data and display each name in a <li> element
-        user.forEach(user => {
-            const listItem =document.createElement('li');
-            listItem.textContent = user.name;
-            userList.appendChild(listItem);
-        });
-        // Append the user list to the data container
-        dataContainer.appendChild(userList);
-    } catch (error) { 
-        //Handle errors by displaying a message
-        dataContainer.innerHTML = 'failed to loa user date.';
 
-    }
-}
-// Invoke the fetchUserData function after the DOMis fully loaded
-fetchUserData();
-});
+    async function fetchUserData() {
+        try { 
+           const fetchUserData = await getfetchUserData();
+           const trigger = await setUpfetchUserData(fetchUserData);
+           return trigger();
+        } catch(error) { 
+            return 'Download!'
+      // Define the APIURL
+      const apiUrl = 'https://jsonplaceholder.typicode.';
+      // Make a GET request fetch(apiUrl)  
+      then(response => {if(!response.ok) { 
+        throw new Error("Network response was not ok");}
+        return response.
+        
+        
+
 
     
         
