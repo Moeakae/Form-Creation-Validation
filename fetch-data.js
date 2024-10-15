88cd83db681fe1dc95c42428f1611d2cfbbb3d65
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-10-15 17:26:20 +0200

    update fetch-data.js

--- fetch-data.js
+++ fetch-data.js
@@ -1,5 +1,6 @@
 
     async function fetchUserData() {
+        try { 
         
            const fetchUserData = await getfetchUserData();
            const record = await setUpfetchUserData(fetchUserData);
@@ -8,23 +9,24 @@
       // Define the APIURL
       const apiUrl = 'https://jsonplaceholder.typicode.';
       // Make a GET request fetch(apiUrl)  
-    } then response => { if(!response,ok) { 
-        throw new Error("Network response was not ok");{ 
+         } .then response => { if(!response,ok) 
+        throw new Error("Network response was not ok"); { 
         return ('response')
-        }
+        }  
     // Select the Data container element
-    const response = await fetch('https://api-data.tracking.com/current.json');
+    const response = await fetch('https://api-data.tracking.com/current.json')
+    .then((response) => response.json(()
+    .then ((data) => console.log(data));
     const record = await response.json();
     document.getElementById('date').innerHTML = record.data[0].date;
     document.getElementById('positive').innerHTML = record.data[0].positive;
     document.getElementById('death').innerHTML = record.data[0].death;
     document.getElementById('deathIncrease').innerHTML = record.data[0].deathIncrease;
       }
-      fetchUserData();
+      fetchUserData(); 
     
     }
-
-
+ 
     
         
     
